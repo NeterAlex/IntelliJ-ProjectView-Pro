f8f9113491bf97833dbda1475e3c75f6487058a7
NeterAlex <neteralex@outlook.com>
2024-10-20 17:58:13 +0800

    feat(chore): 针对部分UI新增拓展方法

---
+++ src/main/kotlin/com/github/neteralex/intellijprojectviewpro/utils/extendFunc.kt
@@ -0,0 +1,103 @@
+package com.github.neteralex.intellijprojectviewpro.utils
+
+import com.github.neteralex.intellijprojectviewpro.model.CompressRule
+import com.github.neteralex.intellijprojectviewpro.state.GlobalState
+import com.intellij.openapi.observable.properties.ObservableMutableProperty
+import com.intellij.openapi.observable.util.bind
+import com.intellij.openapi.observable.util.lockOrSkip
+import com.intellij.openapi.observable.util.transform
+import com.intellij.ui.ColorPanel
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.Cell
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.layout.ComponentPredicate
+import java.awt.Color
+import java.util.concurrent.atomic.AtomicBoolean
+import javax.swing.text.JTextComponent
+import kotlin.reflect.KMutableProperty1
+
+fun Cell<ColorPanel>.bindColor(
+    graphProperty: ObservableMutableProperty<CompressRule?>,
+    property: KMutableProperty1<CompressRule, Color?>
+) =
+    applyToComponent {
+        with(graphProperty) {
+            val mutex = AtomicBoolean()
+
+            selectedColor = get()?.let(property::get)
+
+            graphProperty.afterChange {
+                mutex.lockOrSkip {
+                    selectedColor = it?.let(property::get)
+                }
+            }
+
+            addActionListener {
+                mutex.lockOrSkip {
+                    get()?.let {
+                        property.set(it, selectedColor)
+                        graphProperty.setValue(null, property, it)
+                    }
+                }
+            }
+        }
+    }
+
+fun Cell<JBCheckBox>.bindColorControl(
+    graphProperty: ObservableMutableProperty<CompressRule?>,
+    property: KMutableProperty1<CompressRule, Color?>,
+    defaultValue: Color
+) =
+    applyToComponent {
+        bind(with(graphProperty) {
+            transform(
+                { it?.let(property::get) != null },
+                { selected ->
+                    get()?.apply {
+                        property
+                            .get(this)
+                            .or(defaultValue)
+                            .takeIf { selected }
+                            .also { property.set(this, it) }
+                    }
+                },
+            )
+        })
+    }
+
+fun <T : GlobalState> Cell<JBCheckBox>.bindSelected(
+    graphProperty: ObservableMutableProperty<T>,
+    property: KMutableProperty1<T, Boolean>
+) =
+    bindSelected(with(graphProperty) {
+        transform(
+            { it.let(property::get) },
+            { value -> get().also { property.set(it, value) } }
+        )
+    })
+
+fun <T : JTextComponent> Cell<T>.bindText(
+    graphProperty: ObservableMutableProperty<CompressRule?>,
+    property: KMutableProperty1<CompressRule, String>
+) =
+    bindText(with(graphProperty) {
+        transform(
+            { it?.let(property::get).orEmpty() },
+            { value -> get()?.apply { property.set(this, value) } },
+        )
+    })
+
+fun <T : GlobalState> ObservableMutableProperty<T>.createPredicate(property: KMutableProperty1<T, Boolean>) =
+    object : ComponentPredicate() {
+
+        private val observableProperty = transform(property)
+
+        override fun invoke() = observableProperty.get()
+
+        override fun addListener(listener: (Boolean) -> Unit) = observableProperty.afterChange(listener)
+    }
+
+fun <T> T?.or(other: T): T = this ?: other
+
+fun <T> T?.or(block: () -> T): T = this ?: block()
